79c2bc522e5b29acdcccb7a046be1fa4e400bb7e
Mike Herrera <maherrera@tribune.com>
2014-08-07 15:12:41 -0500

    Renamed member to "deployment" to properly wrap and form good json

--- src/main/java/ext/deployit/michaelherrera/plugin/newrelic/service/GsonNewRelicNoticeWrapper.java
+++ src/main/java/ext/deployit/michaelherrera/plugin/newrelic/service/GsonNewRelicNoticeWrapper.java
@@ -2,14 +2,14 @@ package ext.deployit.michaelherrera.plugin.newrelic.service;
 
 public class GsonNewRelicNoticeWrapper {
 	
-	private NewRelicNotice newRelicNotice;
+	private NewRelicNotice deployment;
 	
 	public GsonNewRelicNoticeWrapper(String app_name, String user, String description)	{
-		this.newRelicNotice = new NewRelicNotice(app_name, user, description);
+		this.deployment = new NewRelicNotice(app_name, user, description);
 	}
 	
-	public NewRelicNotice getNewRelicNotice()	{
-		return this.newRelicNotice;
+	public NewRelicNotice getDeployment()	{
+		return this.deployment;
 	}
 
 }
